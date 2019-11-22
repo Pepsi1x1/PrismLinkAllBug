c519a983827c1fc49c638de09d43aba223dc9bce
David Thompson <davyt23@hotmail.co.uk>
2019-11-22 23:55:39 +0000

    no message

--- LinkAllBug/App.xaml.cs
+++ LinkAllBug/App.xaml.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Prism;
+using Prism.Ioc;
+using Xamarin.Forms.Internals;
+using Incise.Services;
+using System.Threading.Tasks;
+using Prism.Logging;
 
 namespace LinkAllBug
 {
-    public partial class App : Application
+    public partial class App : Prism.DryIoc.PrismApplication
     {
         public App()
         {
@@ -27,5 +33,35 @@ namespace LinkAllBug
         {
             // Handle when your app resumes
         }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //containerRegistry.RegisterForNavigation<SplashScreenPage>();
+        }
+
+        protected override async void OnInitialized()
+        {
+            this.InitializeComponent();
+            Xamarin.Forms.Device.SetFlags(new[] { "Visual_Experimental", "CollectionView_Experimental" });
+
+#if DEBUG
+            //HotReloader.Current.Run(this);
+            // Handle Xamarin Form Logging events such as Binding Errors
+            Log.Listeners.Add(new TraceLogListener());
+#endif
+
+            this.LogUnobservedTaskExceptions();
+
+            await base.NavigationService.NavigateAsync("SplashScreenPage");
+        }
+
+        private void LogUnobservedTaskExceptions()
+        {
+            TaskScheduler.UnobservedTaskException += (sender, e) =>
+            {
+                base.Container.Resolve<ILoggerFacade>().Log(e.Exception.ToString(), Category.Exception, Priority.None);
+            };
+        }
     }
+
 }
